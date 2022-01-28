5526a8b81b8232b8a2e366dbbcaeadae165f3d1f
sean <sean@roseycor.com>
2022-01-27 19:46:38 -0800

    cool trick for histogram

--- Histogram.py
+++ Histogram.py
@@ -21,13 +21,17 @@
 # histogram([4,2,1,5,100])
 
 def histogram(list):
-  for n in list:
-    output = ''
-    number = n
-    while (number > 0):
-      output += '*'
-      number = number - 1
-    print(output)
+  for i in list:
+    print(i*'*')
+
+    
+  # for n in list:
+  #   output = ''
+  #   number = n
+  #   while (number > 0):
+  #     output += '*'
+  #     number = number - 1
+  #   print(output)
 
 histogram([1,2,3,4,5,6,7,8,9])
 
