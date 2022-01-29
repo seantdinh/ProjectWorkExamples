0fd7db58e57a2ec7462f08f83fdb8ee11b207a53
sean <sean@roseycor.com>
2022-01-28 23:15:52 -0800

    lets get it nulls

--- GlassDoorInterviewQuestions/replacingnonelastcharacter.py
+++ GlassDoorInterviewQuestions/replacingnonelastcharacter.py
@@ -1,6 +1,19 @@
+
 def none_replace(ls):
-    p = None
-    return [p:=e if e is not None else p for e in ls]
+    t = 0
+    rs = []
+    for i in ls:
+        if i is not None:
+            rs.append(i)
+            t = i
+        else:
+            rs.append(t)
+    
+    for i, x in enumerate(rs):
+        if x == 0:
+            rs[i] = None
+
+    return rs
 
 stuffies = [None, None, 1, 2, None, None, 3, 4, None, 5, None, None]
 
